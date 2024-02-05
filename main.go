d95ab02741c52a514144e90c1423be65dd09f858
Fabio Caumo <fabio.caumo@dieboldnixdorf.com>
2024-02-05 08:50:07 -0300

    Adding debug config

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/fabiovpcaumo/terraform-provider-mongodb/mongodb"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debug,
+		ProviderAddr: "registry.terraform.io/fabiovpcaumo/mongodb",
 		ProviderFunc: mongodb.Provider,
-	})
+	}
+
+	plugin.Serve(opts)
 }
